17e645df31019f326c0d78872eb11b1b885b85bc
Dan Halverson <daniel.m.halverson@gmail.com>
2016-02-28 14:31:22 -0600

    beautify

--- app/photoGallery.js
+++ app/photoGallery.js
@@ -1,120 +1,120 @@
 'use strict';
 var app = angular.module('PhotoGallery', ['ui.bootstrap']);
-app.controller('PhotoGalleryController', function ($scope, $uibModal) {
+app.controller('PhotoGalleryController', function($scope, $uibModal) {
 
-    $scope.buildArray = function (pageNumber) {
-        $scope.photoArray = [];
-        for (var i = $scope.firstPhoto[pageNumber - 1]; i <= $scope.lastPhoto[pageNumber - 1]; i++) {
-            var source = $scope.filePrefix + i + $scope.fileSuffix;
-            $scope.photoArray.push(source);
-        }
-    };
-
-    $scope.click = function (pageNumber) {
-        $scope.buildArray(pageNumber);
-    };
-
-    $scope.init = function () {
-        var perPage = Number($scope.perPage);
-        var totalPhotos = Number($scope.totalPhotos);
-        $scope.photoArray = [];
-        $scope.buttons = [];
-        // determine the first photo and the last photo to appear on each page/button.
-        // the equal division of photos results in the number of buttons needed.
-        $scope.firstPhoto = [];
-        $scope.lastPhoto = [];
-        $scope.go = true;
-        $scope.page = 0;
-
-        while ($scope.go) {
-            if ($scope.page === 0) {
-                $scope.firstPhoto.push(1);
-                $scope.lastPhoto.push(perPage);
-            } else {
-                var nextFirst = $scope.lastPhoto[$scope.page - 1] + 1;
-                var nextLast = nextFirst + perPage;
-                if (nextLast > totalPhotos) {
-                    nextLast = totalPhotos;
-                }
-                $scope.firstPhoto.push(nextFirst);
-                $scope.lastPhoto.push(nextLast);
-            }
-
-            $scope.buttons.push($scope.page + 1);
-            if ($scope.lastPhoto[$scope.page] === totalPhotos) {
-                $scope.go = false;
-            } else {
-                $scope.page++;
-            }
-        }
-        $scope.buildArray(1);
-    };
-
-    $scope.clickAction = function ($index) {
-        $uibModal.open({
-            animation: false,
-            templateUrl: './photoModal.html',
-            controller: 'PhotoGalleryModalController',
-            size: 'sm',
-            resolve: {
-                photo: function () {
-                    return {
-                        "index": $index,
-                        "photoArray": $scope.photoArray
-                    };
-                }
-            }
-        });
-    };
-
-    $scope.init();
-
-});
-app.directive('photoGalleryDirective', function () {
-    return {
-        restrict: 'E',
-        scope: {
-            perPage: '@',
-            totalPhotos: '@',
-            filePrefix: '@',
-            fileSuffix: '@'
-        },
-        templateUrl: './photoTemplate.html',
-        controller: 'PhotoGalleryController'
+  $scope.buildArray = function(pageNumber) {
+    $scope.photoArray = [];
+    for (var i = $scope.firstPhoto[pageNumber - 1]; i <= $scope.lastPhoto[pageNumber - 1]; i++) {
+      var source = $scope.filePrefix + i + $scope.fileSuffix;
+      $scope.photoArray.push(source);
     }
+  };
+
+  $scope.click = function(pageNumber) {
+    $scope.buildArray(pageNumber);
+  };
+
+  $scope.init = function() {
+    var perPage = Number($scope.perPage);
+    var totalPhotos = Number($scope.totalPhotos);
+    $scope.photoArray = [];
+    $scope.buttons = [];
+    // determine the first photo and the last photo to appear on each page/button.
+    // the equal division of photos results in the number of buttons needed.
+    $scope.firstPhoto = [];
+    $scope.lastPhoto = [];
+    $scope.go = true;
+    $scope.page = 0;
+
+    while ($scope.go) {
+      if ($scope.page === 0) {
+        $scope.firstPhoto.push(1);
+        $scope.lastPhoto.push(perPage);
+      } else {
+        var nextFirst = $scope.lastPhoto[$scope.page - 1] + 1;
+        var nextLast = nextFirst + perPage;
+        if (nextLast > totalPhotos) {
+          nextLast = totalPhotos;
+        }
+        $scope.firstPhoto.push(nextFirst);
+        $scope.lastPhoto.push(nextLast);
+      }
+
+      $scope.buttons.push($scope.page + 1);
+      if ($scope.lastPhoto[$scope.page] === totalPhotos) {
+        $scope.go = false;
+      } else {
+        $scope.page++;
+      }
+    }
+    $scope.buildArray(1);
+  };
+
+  $scope.clickAction = function($index) {
+    $uibModal.open({
+      animation: false,
+      templateUrl: './photoModal.html',
+      controller: 'PhotoGalleryModalController',
+      size: 'sm',
+      resolve: {
+        photo: function() {
+          return {
+            "index": $index,
+            "photoArray": $scope.photoArray
+          };
+        }
+      }
+    });
+  };
+
+  $scope.init();
+
 });
-app.controller('PhotoGalleryModalController', function ($scope, $uibModalInstance, photo) {
+app.directive('photoGalleryDirective', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      perPage: '@',
+      totalPhotos: '@',
+      filePrefix: '@',
+      fileSuffix: '@'
+    },
+    templateUrl: './photoTemplate.html',
+    controller: 'PhotoGalleryController'
+  }
+});
+app.controller('PhotoGalleryModalController', function($scope, $uibModalInstance, photo) {
 
-    $scope.init = function () {
-        $scope.photo = photo;
-        $scope.photoIndex = Number(photo.index);
-        $scope.photoArray = photo.photoArray;
-        $scope.showHideButtons();
-    };
+  $scope.init = function() {
+    $scope.photo = photo;
+    $scope.photoIndex = Number(photo.index);
+    $scope.photoArray = photo.photoArray;
+    $scope.showHideButtons();
+  };
 
-    $scope.showHideButtons = function () {
-        $scope.modalPhoto = $scope.photoArray[$scope.photoIndex];
-        if ($scope.photoIndex === 0) {
-            $scope.hidePrevious = true;
-        } else {
-            $scope.hidePrevious = false;
-        }
-        if ($scope.photoIndex === ($scope.photoArray.length - 1)) {
-            $scope.hideNext = true;
-        } else {
-            $scope.hideNext = false;
-        }
-    };
+  $scope.showHideButtons = function() {
+    $scope.modalPhoto = $scope.photoArray[$scope.photoIndex];
+    if ($scope.photoIndex === 0) {
+      $scope.hidePrevious = true;
+    } else {
+      $scope.hidePrevious = false;
+    }
+    if ($scope.photoIndex === ($scope.photoArray.length - 1)) {
+      $scope.hideNext = true;
+    } else {
+      $scope.hideNext = false;
+    }
+  };
 
-    $scope.prev = function () {
-        $scope.photoIndex--;
-        $scope.showHideButtons();
-    };
+  $scope.prev = function() {
+    $scope.photoIndex--;
+    $scope.showHideButtons();
+  };
 
-    $scope.next = function () {
-        $scope.photoIndex++;
-        $scope.showHideButtons();
-    };
+  $scope.next = function() {
+    $scope.photoIndex++;
+    $scope.showHideButtons();
+  };
 
-    $scope.init();
+  $scope.init();
 });
--- server.js
+++ server.js
@@ -7,12 +7,12 @@ var port = 3000;
 
 app.use(express.static(__dirname + '/app'));
 app.get('*', function response(req, res) {
-    res.sendFile(path.join(__dirname, 'app/index.html'));
+  res.sendFile(path.join(__dirname, 'app/index.html'));
 });
 
 app.listen(port, '0.0.0.0', function onStart(err) {
-    if (err) {
-        console.log(err);
-    }
-    console.info('==> Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
+  if (err) {
+    console.log(err);
+  }
+  console.info('==> Listening on port %s. Open up http://0.0.0.0:%s/ in your browser.', port, port);
 });
